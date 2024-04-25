8a8a656757a6818c827ce0b1e5d18a64c1d3981e
kmsssn <aminaashim0@gmail.com>
2024-04-25 19:23:42 +0500

    Initial commit

---
+++ Java Classes(Files)/coursemanagementdb.sql
@@ -0,0 +1,111 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Apr 25, 2024 at 02:21 PM
+-- Server version: 5.7.24
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `coursemanagementdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `adminlogin`
+--
+
+CREATE TABLE `adminlogin` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `adminlogin`
+--
+
+INSERT INTO `adminlogin` (`username`, `password`) VALUES
+('kmsssn', 'a23092004');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lecturerlogin`
+--
+
+CREATE TABLE `lecturerlogin` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `lecturerlogin`
+--
+
+INSERT INTO `lecturerlogin` (`username`, `password`, `firstname`, `lastname`) VALUES
+('kanatnuris', 'k1234567', 'Nurislam', 'Kanatuly');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `studentlogin`
+--
+
+CREATE TABLE `studentlogin` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `idnumber` int(11) NOT NULL,
+  `grade` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `studentlogin`
+--
+
+INSERT INTO `studentlogin` (`username`, `password`, `firstname`, `lastname`, `idnumber`, `grade`) VALUES
+('default1', 'default1', 'Default', 'Student', 11111, '95,80,'),
+('default2', 'default2', 'Default', 'Studentt', 11112, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `adminlogin`
+--
+ALTER TABLE `adminlogin`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `lecturerlogin`
+--
+ALTER TABLE `lecturerlogin`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `studentlogin`
+--
+ALTER TABLE `studentlogin`
+  ADD PRIMARY KEY (`idnumber`),
+  ADD UNIQUE KEY `username` (`username`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
